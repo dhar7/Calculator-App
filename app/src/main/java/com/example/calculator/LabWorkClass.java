cc310e0973116146b29064c7067d3619d1eba105
dhar7 <dhar1707069@stud.kuet.ac.bd>
2021-11-28 08:47:34 +0600

    Java class added

---
+++ app/src/main/java/com/example/calculator/LabWorkClass.java
@@ -0,0 +1,19 @@
+package com.example.calculator;
+
+public class LabWorkClass
+{
+    int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public LabWorkClass(int a)
+    {
+        this.a = a;
+    }
+}
